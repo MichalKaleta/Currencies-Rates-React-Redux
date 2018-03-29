45e2c07abfcdcebecd4308f9f9961d25c7e99da9
Michał Kaleta <mic.kaleta@gmail.com>
2018-03-29 22:57:26 +0200

    calc amount,  ready string

--- src/components/form.js
+++ src/components/form.js
@@ -2,10 +2,15 @@ import React ,{Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'
 
-import  {apiRequest} from '../actions'
+import  {apiRequest} from '../actions';
+import {yesterday, firstOfMonth } from './dates'
 
 class Form extends Component{
   
+  componentDidMount(){
+      this.props.apiRequest('a','eur',firstOfMonth,yesterday)
+    }
+
   fetchData(ev){
     ev.preventDefault();
     var currency = ev.target[0].value;
@@ -16,10 +21,9 @@ class Form extends Component{
   }
   
   render(){
+    
     return (
       <form  onSubmit={ this.fetchData.bind(this) }  className="form-row">
-      
-          
           <div className="col-sm-3">
             <select className='form-control'>
               <option value="eur">EUR</option>
@@ -30,19 +34,20 @@ class Form extends Component{
            </div>
            
            <div className="col-sm-3">
-            <input name='fromDate' id='fromDate' type='date'  className="form-control"/>
+            <input name='fromDate' id='fromDate' type='date'  className="form-control"
+                    defaultValue={firstOfMonth}/>
           </div>
           
 
           <div className="col-sm-3">
-            <input name="toDate" id="toDate" type='date'  className="form-control"/>
+            <input name="toDate" id="toDate" type='date'  className="form-control"
+                    max= {yesterday} defaultValue={yesterday} />
           </div>
 
           <div className="col-sm-3">
             <input  type='submit' value='click' id='btn-submit' className='form-control btn btn-inline btn-success'/>
           </div>
-        
-       
+
       </form>
     )
   }
